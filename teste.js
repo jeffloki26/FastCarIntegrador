a7182180dde652e1ef32d82bfaf3bb70a8fba844
ThomazGattas <thomazgattas@gmail.com>
2022-05-17 16:16:09 -0300

    produtos

--- teste.js
+++ teste.js
@@ -21,34 +21,28 @@ const Inserir = [
   },
   {
     "imagem": "1fcf978d400da66f8bae58b6d4916668",
-    "nome": "Frisbee plástico",
-    "descricao": "Um frisbee simples de plástico",
-    "valor": 19
+    "nome": "Tapete Carpete Ferrari Testarossa 84 A",
+    "descricao": "A melhor qualidade em tapetes automotivos",
+    "valor": 899
   },
   {
     "imagem": "b8d8bdf69cf4f5dd0fafb4f94a94413e",
-    "nome": "Ração fórmula natural",
-    "descricao": "Ração da marca Fórmula Natural",
-    "valor": 60
+    "nome": "Mercedes-Benz C 200",
+    "descricao": "O novo Classe C une esportividade, elegância e minimalismo",
+    "valor": 349.900
   },
   {
     "imagem": "1f22ec60dd840553513f9d68176802d0",
-    "nome": "Ração Nutrilus",
-    "descricao": "Ração simples da marca Nutrilus",
-    "valor": 65
+    "nome": "Blindagem e assistência",
+    "descricao": "Profissionais altamente qualificados para a blindagem do seu veículo.",
+    "valor": 54.900
   },
   {
     "imagem": "7a74b37cf635e8050c97fec09e99d799",
-    "nome": "Ração True",
-    "descricao": "Ração da marca True",
-    "valor": 49
+    "nome": "Pintura Automotiva",
+    "descricao": "Pintura com alta qualidade, usando tinta ecologicamente correta.",
+    "valor": 3000
   },
-  {
-    "imagem": "5b9a1863992e1ed6d6262c18591affd1",
-    "nome": "Sorvete canino",
-    "descricao": "Um sorvete para seu cão",
-    "valor": 55
-  }
 ]
 
 async function app() {
